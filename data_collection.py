292c2e138c9f4f71ee42906d1cd8cf003f6718cf
Ijjaz Khan <ijazkhan19932@gmail.com>
2022-12-10 18:33:10 +0500

    (Data Cleaning ) salaries
    (Data Cleaning ) salaries

--- data_collection.py
+++
@@ -1,8 +0,0 @@
-import glassdoor_scraper as gs 
-import pandas as pd 
-
-path = "D:/GitHub/data-Scientist-salaries-projects/chromedriver"
-
-df = gs.get_jobs('data scientist',1000, False, path, 15)
-
-df.to_csv('glassdoor_jobs.csv', index = False)
